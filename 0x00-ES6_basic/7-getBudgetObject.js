6573dd4968eb63dc07a7489c49e7fd50fcac2c6f
mo7amed3mad17 <memo12317emad@gmail.com>
2024-09-30 23:59:35 +0200

    all_after_revision

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,18 @@
+export default function getBudgetObject(income, gdp, capita) {
+    const budget = {
+      income,
+      gdp,
+      capita,
+    };
+  
+    return budget;
+  }
+  export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}

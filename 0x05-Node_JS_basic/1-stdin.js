4c6809c5cf807452f36f1c90f46c3274f04c461a
mo7amed3mad17 <memo12317emad@gmail.com>
2024-12-08 23:46:21 +0200

    1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,22 @@
-// 1-stdin.js
+// Display welcome message
 process.stdout.write('Welcome to ALX, what is your name?\n');
 
+// Set encoding for stdin to handle text input
 process.stdin.setEncoding('utf8');
 
+// Event listener for data input
 process.stdin.on('data', (input) => {
-    const name = input.trim();
-    console.log(`Your name is: ${name}`);
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
+  const name = input.trim();
+  process.stdout.write(`Your name is: ${name}\n`);
 });
 
+// Event listener for end of input (EOF)
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
+
+// Handle program termination (e.g., when the user presses Ctrl+C)
+process.on('SIGINT', () => {
+  process.stdout.write('\nThis important software is now closing\n');
+  process.exit();
+});

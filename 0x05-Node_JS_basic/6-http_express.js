115217ce194222baf8905f1a2a30078201403984
Mohamed Emad <memo12317emad@gmail.com>
2024-12-09 09:34:46 +0200

    6

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello ALX!');
+});
+
+app.listen(1245, () => {
+  console.log('Server is running on port 1245');
+});
+
+module.exports = app;
